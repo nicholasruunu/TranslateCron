f7357abc55a9d79f12043d8157f363fdcbcd10d8
Jonathan Garbee <jonathan@garbee.me>
2015-03-19 14:51:12 -0400

    Add test for making a valid object.

--- tests/CronTest.php
+++ tests/CronTest.php
@@ -9,4 +9,15 @@ class CronTest extends \PHPUnit_Framework_TestCase {
     public function testExceptionIsRaisedForInvalidCronArgument() {
         new Cron('* *');
     }
+
+    /**
+     * @covers \Garbee\TranslateCron\Cron::__construct
+     */
+    public function testObjectCanBeConstructedWithValidArgument() {
+        $cron = new Cron('*, *, 3, *, *');
+
+        $this->assertInstanceOf(Cron::class, $cron);
+
+        return $cron;
+    }
 }
